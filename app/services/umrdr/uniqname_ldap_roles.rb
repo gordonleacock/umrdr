b8104a8af7298bdcf159d57591834c4a05af5dcc
Gordon Leacock <gordonl@umich.edu>
2016-04-01 12:01:35 -0400

    fix to return false when uniqname is not found add config via the Umrdr::LdapConfig class

--- app/services/umrdr/uniqname_ldap_roles.rb
+++ app/services/umrdr/uniqname_ldap_roles.rb
@@ -1,37 +1,24 @@
 module Umrdr
 
+  # For local development you can use the https://www.incommon.org/cert/repository/InCommonServerCA.txt
+  # as a certiticate for verifying the MCommunity LDAP server's certificate.
+  # ENV['LDAPTLS_CACERT'] = '/www/www.lib/misc/bundle.crt';
+
   require 'rubygems'
   require 'net/ldap'
-
-# For local development you can use the https://www.incommon.org/cert/repository/InCommonServerCA.txt
-# as a certiticate for verifying the MCommunity LDAP server's certificate.
-# ENV['LDAPTLS_CACERT'] = '/www/www.lib/misc/bundle.crt';
 
   class UniqnameLdapRoles
 
     attr_accessor :uniqname
-
-    HOST = 'ldap.umich.edu'
-    PORT = 636
-    ENCRYPTION = :simple_tls
-    USERNAME =  'mcommunityuser'
-    PASSWORD = 'mcommunitypass'
-    AUTH = {:method => :simple, :username => USERNAME, :password => PASSWORD}
 
     def self.uniqname_faculty_or_staff?(uname)
       Umrdr::UniqnameLdapRoles.new(uname).run
     end
 
     def initialize(uname)
+      # set uniqname for search
       @uniqname = uname
     end
-
-    # Errors returned rescue from ldap.open in run method can include
-    # "getaddrinfo: nodename nor servname provided, or not known" when HOST is wrong
-    # "Operation timed out - user specified timeout" when PORT is wrong
-    # "undefined method `umichinstroles' for #<Net::LDAP::Entry:OBJECT-NUMBER>" when  USERNAME is wrong
-    # "undefined method `umichinstroles' for #<Net::LDAP::Entry:OBJECT-NUMBER>" when  PASSWORD is wrong
-    # "Error: Invalid binding information" when the AUTH is wrong
 
     def faculty_or_staff_roles?(umichinstroles)
       faculty = staff = student = false
@@ -52,9 +39,16 @@ module Umrdr
 
     end
 
+    # Errors returned rescue from ldap.open in run method can include
+    # "getaddrinfo: nodename nor servname provided, or not known" when HOST is wrong
+    # "Operation timed out - user specified timeout" when PORT is wrong
+    # "undefined method `umichinstroles' for #<Net::LDAP::Entry:OBJECT-NUMBER>" when  USERNAME is wrong
+    # "undefined method `umichinstroles' for #<Net::LDAP::Entry:OBJECT-NUMBER>" when  PASSWORD is wrong
+    # "Error: Invalid binding information" when the AUTH is wrong
+
     def run
       begin
-        @ldap = Net::LDAP.open(:host => HOST, :port => PORT, :encryption => ENCRYPTION, :auth => AUTH) do |ldap|
+        @ldap = Net::LDAP.open(:host => Umrdr::LdapConfig.host, :port => Umrdr::LdapConfig.port, :encryption => Umrdr::LdapConfig.encryption, :auth => Umrdr::LdapConfig.auth) do |ldap|
           # set up search
           filter = Net::LDAP::Filter.eq('objectclass', '*')
           treebase = "uid=#{@uniqname},ou=People,dc=umich,dc=edu"
@@ -64,6 +58,8 @@ module Umrdr
             return faculty_or_staff_roles?(entry.umichinstroles)
 
           end # do |entry|
+
+          return false  # for the case when there is no entry, e.g. uniqname zzzzzz
         end # do |ldap|
 
       rescue Exception, StandardError, Net::LDAP::ConnectionError, Net::LDAP::Error, SocketError, SystemCallError, OpenSSL::SSL::SSLError => e
